f279a2dc8900a95dc8df39a3d12647153f4d23bb
Tom Nash <tommynosh@hotmail.com>
2013-01-13 11:29:08 -0500

    update pages

--- php/save_idea.php
+++ php/save_idea.php
@@ -3,32 +3,26 @@ $ideaName = $_POST["ideaName"];
 $ideaDesc = $_POST["ideaDescription"];
 $skills = $_POST["iSkills"];
 $tags = $_POST["iTags"];
-foreach ($_POST as $value)
+if (isset($_POST["submit"]))
 {
-    if (!isset($value)) 
-    { ?>
-    <html>
-    <body>
-    Please fill in all of the forms
-    <?php $formComplete = False; ?>
-    </body>
-    </html>
-
-    <?php }
-
+    foreach ($_POST as $value)
+    {
+        if (!empty($value))
+        {      
+        header('Location: http://think_do.morelazers_1.c9.io/html/incomplete_idea.html');
+        }
+    }
+    if ($formComplete)
+    {
+    /*
+    *
+    *Save idea to the database
+    *
+    */
+    }
 }
-if ($formComplete)
-{ ?>
-
-<html>
-<body>
-
-Idea title: <?php echo $_POST["ideaName"]; ?> <br>
-Description: <?php echo $_POST["ideaDescription"]; ?>
-
-</body>
-</html>
-
-<?php
+else
+{
+    
 }
 ?>
