9353ebe1e4dd91c764c18d484a9e88bdcb92dc24
Nathan Emery <nathanemery2@gmail.com>
2013-01-21 01:55:10 +0000

    Changed parameter presence check
    Changed the method for checking the presence of the 'pid' parameter to
    avoid mysql notices being output. Also added some commenting to the
    code.

--- php/view_ideas.php
+++ php/view_ideas.php
@@ -10,24 +10,28 @@
 
 <?php
 function getIdea(){
+	//Connect to mysql
 	$con = mysql_connect("127.0.0.1:3306","root","");
-	
+	//If connection failed, report error
 	if (!$con)
 	{
 		die('Could not connect to mysql: ' . mysql_error());
 	}
-	if($_GET["pid"]!=null)
+	//Check for 'pid' parameter in URL
+	if(array_key_exists("pid", $_GET))
 	{
+		//If present, select thinkdo database
 		mysql_select_db("thinkdo", $con);
-		
 		$projectID = $_GET["pid"];
 		$project = mysql_query("SELECT * FROM project WHERE projectID=" . $projectID);
+		//Get project data for the project from the database
 		$projectArray = mysql_fetch_array($project);
+		//Assign query results to variables
 		$pName = $projectArray['projectName'];
 		$pDesc = $projectArray['description'];
 		$pSkills = $projectArray['skillsRequired'];
 		$pTags = $projectArray['tags'];
-		
+		//Output project information with appropriate markup
 		echo "<h2>".$pName."</h2>";
 		echo "<p>".$pDesc."</p>";
 	}
