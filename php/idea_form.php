8539d2b561c6fc023a9cb830f5af4c39e114bbcc
Nathan Emery <nathanemery2@gmail.com>
2013-01-21 03:30:55 +0000

    Added privacy setting to submit idea
    Added radio buttons to the submit idea form to allow a user to select
    either public or private status for their project.

--- php/idea_form.php
+++ php/idea_form.php
@@ -31,8 +31,22 @@
         { 
             $iName = $_POST["ideaName"];
             $iDesc = $_POST["ideaDescription"];
-            $sql="INSERT INTO project (projectName, description) VALUES ('".$iName."', '".$iDesc."')";
+			$iSkills = $_POST["iSkills"];
+			$iTags = $_POST["iTags"];
+			$iDate = date("Y-m-d H:i:s");
+			//Convert radio button value to integer representation of boolean
+			if($_POST["iPrivacy"]=="public")
+			{
+				$iOpen = 1;
+			}
+			else
+			{
+				$iOpen = 0;
+			}
+			//Execute query to insert new tuple
+            $sql="INSERT INTO project (projectName, description, skillsRequired, tags, dateCreated, isOpen) VALUES ('".$iName."', '".$iDesc."', '".$iSkills."', '".$iTags."', '".$iDate."', '".$iOpen."')";
             if (!mysql_query($sql, $con))
+			//If error durying query execution report error
             {
                 echo 'failed to add record';
                 die('Error: ' . mysql_error());
@@ -49,24 +63,58 @@
     
     function showForm() 
     {
+		/*
+		This function echos the idea input form to the page. If a value was previously entered into an input
+		it is echoed back into that input to remove the need to reenter data in the case of a field being
+		invalid.
+		*/
         echo '<form method="post" action="'; echo $PHP_SELF; echo '">
               <label for="idea_title">Name your idea:</label><br>
               <input type="text" name="ideaName" id="idea_title" value="';
-              echo $_POST["ideaName"];
-              echo '"><br>
+			echo $_POST["ideaName"];
+            echo '"><br>
               <label for="idea_desc">Describe your idea:</label><br>
               <input type="text" name="ideaDescription" id="idea_desc" size="50" value="';
-              echo $_POST["ideaDescription"]; 
-              echo '"><br>
+            echo $_POST["ideaDescription"]; 
+            echo '"><br>
               <label for="skills">Beneficial Skills:</label><br>
               <input type="text" name="iSkills" id="skills" value="';
-              echo $_POST["iSkills"];
-              echo '"><br>
+            echo $_POST["iSkills"];
+            echo '"><br>
               <label for="tags">Tags:</label><br>
               <input type="text" name="iTags" id="tags" value="';
-              echo $_POST["iTags"];
-              echo '"><br>
-              <input type="submit" name="submit" value="Submit">
-              </form>';
+            echo $_POST["iTags"];
+			echo '"><br><label for="Privacy">Privacy:</label><br>';
+			//This if is responsible for preselecting a one of the radio buttons for privacy
+			if(array_key_exists("iPrivacy", $_POST))
+			{
+				if($_POST["iPrivacy"] == "public")
+				{
+					echo '
+					  <input type="radio" name="iPrivacy" id="privacy" value="public" checked="checked">Public';
+				}
+				else
+				{
+					echo '
+					  <input type="radio" name="iPrivacy" id="privacy" value="public">Public';
+				}
+				if($_POST["iPrivacy"] == "private")
+				{
+					echo '
+					  <input type="radio" name="iPrivacy" id="privacy" value="private" checked="checked">Private';
+				}
+				else
+				{
+					echo '
+					  <input type="radio" name="iPrivacy" id="privacy" value="private">Private';
+				}
+				
+			}
+			else
+			{
+				echo '<input type="radio" name="iPrivacy" id="privacy" value="public" checked="checked">Public
+					<input type="radio" name="iPrivacy" id="privacy" value="private">Private';
+			}
+			echo '<br><input type="submit" name="submit" value="Submit"></form>';
     }
 ?> 
