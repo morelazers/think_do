fc50036cfc49fbc8aaff4fcae89344e6b08717da
Nathan Emery <nathanemery2@gmail.com>
2013-01-21 00:10:44 +0000

    Implemented basic view idea
    Implemented a basic version of the view idea page. Only displays project
    name and description, tags and skills not yet displayed.

--- php/view_ideas.php
+++ php/view_ideas.php
@@ -2,8 +2,34 @@
     <div class="clear"></div>
         <div id="post-container">
         	<div class="post">
-            Ideas lifted from the database are displayed here
+            <?php getIdea(); ?>
             </div>
         </div>
     </div>
 <?php include 'footer.php'; ?>
+
+<?php
+function getIdea(){
+	$con = mysql_connect("127.0.0.1:3306","root","");
+	
+	if (!$con)
+	{
+		die('Could not connect to mysql: ' . mysql_error());
+	}
+	if($_GET["pid"]!=null)
+	{
+		mysql_select_db("thinkdo", $con);
+		
+		$projectID = $_GET["pid"];
+		$project = mysql_query("SELECT * FROM project WHERE projectID=" . $projectID);
+		$projectArray = mysql_fetch_array($project);
+		$pName = $projectArray['projectName'];
+		$pDesc = $projectArray['description'];
+		$pSkills = $projectArray['skillsRequired'];
+		$pTags = $projectArray['tags'];
+		
+		echo "<h2>".$pName."</h2>";
+		echo "<p>".$pDesc."</p>";
+	}
+}
+?>
