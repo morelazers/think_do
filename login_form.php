c808ed26e44b964bb1cc29aff11e161fbfdc723c
Tom Nash <tommynosh@hotmail.com>
2013-01-24 22:48:44 +0000

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -85,33 +85,23 @@
         $user = mysql_fetch_assoc($resultRow);
         $storedPass = $user['password'];
         
-        function encrypt($str, $key)
-	{
-		$block = mcrypt_get_block_size('des', 'ecb');
-		$pad = $block - (strlen($str) % $block);
-		$str .= str_repeat(chr($pad), $pad);
-		return mcrypt_encrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
-	}
-	
-	$checkString = encrypt($p, $k);
-        
         //Decrypt the database password and check if it is equal to the one inputted
-        /*function decrypt($str, $key)
+        function decrypt($str, $key)
         {   
             	$str = mcrypt_decrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
     		$block = mcrypt_get_block_size('des', 'ecb');
     		$pad = ord($str[($len = strlen($str)) - 1]);
     		return substr($str, 0, strlen($str) - $pad);
-	}*/
+	}
 	
-        //$decryptedPass = decrypt($storedPass, $k);
+        $decryptedPass = decrypt($storedPass, $k);
         
         echo $sql;
         echo '<br>';
         echo $storedPass;
         echo '<br>';
-        echo $checkString;
+        echo $decryptedPass;
         echo '<br>';
-        return ($storedPass == $checkString);
+        return ($decrypted == $p);
     }
 ?> 
