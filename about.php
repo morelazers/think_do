57a32bf7aa77dba38803ab0258774cc75c64ce7a
Tom Nash <tommynosh@hotmail.com>
2013-01-29 13:24:48 +0000

    Update about.php

--- about.php
+++ about.php
@@ -2,8 +2,8 @@
 	/**
 		Author: Tom Nash
 	*/
-<?php include 'header.php'; ?>
-    <div class="clear"></div>
+include 'header.php'; ?>
+<div class="clear"></div>
 	<div id="post-container">
 		<div class="post">
 			<p>We are an ideas-sharing website, designed to get you in touch with the people who are thinking about tomorrow</p>
