e20e54cbefd8fae9340693d58bec969c746688e0
Tom Nash <tommynosh@hotmail.com>
2013-01-31 15:51:09 +0000

    Update mysql_functions.php

--- mysql_functions.php
+++ mysql_functions.php
@@ -119,7 +119,6 @@ function userIsNotTaken($u, $c)
   	//If username is not taken, add new user to database
    	if($userTaken == null)
     {
-		echo 'user is not taken';
         return true;
     }
     else
