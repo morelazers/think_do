304e2d57c94c29386b51f87809c1b1a14d7d5398
Tom Nash <tommynosh@hotmail.com>
2013-01-31 06:26:25 -0800

    Create modify_profile.php

---
+++ modify_profile.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+    <div class="clear"></div>
+        <div id="post-container">
+          <div class="post">
+            <?php include 'profile_form.php'; ?>
+            </div>
+        </div>
+    </div>
+<?php include 'footer.php'; ?>
+
