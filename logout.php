110e5b0b144b86008bc195a64a0f68133d9b56d7
Tom Nash <tommynosh@hotmail.com>
2013-02-04 11:40:50 +0000

    fixed broken syntax. damn those missing brackets
    need an IDEEEEEEEEEEEEEEEEE

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-                	if (isset($_SESSION['usr']))
-                	{
-                		unset($_SESSION['usr']);
-                		session_destroy();
-                	}
+if (isset($_SESSION['usr']))
+{
+                unset($_SESSION['usr']);
+                session_destroy();
+}
 header('Location: index.php');
 ?>
