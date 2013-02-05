a23912b03b64aaa415a8511f7518c2b60282a072
Tom Nash <tommynosh@hotmail.com>
2013-02-05 21:35:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ include 'header.php'; ?>
 	<div id="post-container">
 		<div class="post">
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
+			<?php
+			if (isset($_SESSION['usr']))
+			{
+				getUserInterests($_SESSION['usr']);	
+			}
+			?>
 		</div>			
 	</div>	
 </div>
