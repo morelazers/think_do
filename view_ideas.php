8e4e819a53f2dcfc230554ce3a439c8c956f1db1
Tom Nash <tommynosh@hotmail.com>
2013-02-05 12:30:45 +0000

    Update view_ideas.php

--- view_ideas.php
+++ view_ideas.php
@@ -19,23 +19,23 @@ function getIdea(){
 	//Check for 'pid' parameter in URL
 	if(array_key_exists("pid", $_GET))
 	{
-		$projectID = $_GET["pid"];
-		$project = mysql_query("SELECT * FROM project WHERE projectID=" . $projectID);
+		$ideaID = $_GET["pid"];
+		$idea = mysql_query("SELECT * FROM idea WHERE ideaID =" . $ideaID);
 		//Get project data for the project from the database
-		$projectArray = mysql_fetch_array($project);
+		$ideaArray = mysql_fetch_array($idea);
 		//Assign query results to variables
-		$pName = $projectArray['projectName'];
-		$pDesc = $projectArray['description'];
-		$pSkills = $projectArray['skillsRequired'];
-		$pTags = $projectArray['tags'];
+		$iName = $ideaArray['projectName'];
+		$iDesc = $ideaArray['description'];
+		$iSkills = $ideaArray['skillsRequired'];
+		$iTags = $ideaArray['tags'];
 		//Output project information with appropriate markup
-		echo '<h2>'.$pName.'</h2><br>';
-		echo '<h3>Project Description:</h3><br>';
-		echo '<p>'.$pDesc.'</p><br>';
+		echo '<h2>'.$iName.'</h2><br>';
+		echo '<h3>Idea Description:</h3><br>';
+		echo '<p>'.$iDesc.'</p><br>';
 		echo '<h3>Skills Needed:</h3><br>';
-		echo '<p>'.$pSkills.'</p><br>';
-		echo '<h3>Project Tags:</h3><br>';
-		echo '<p>'.$pTags.'</p>';
+		echo '<p>'.$iSkills.'</p><br>';
+		echo '<h3>Idea Tags:</h3><br>';
+		echo '<p>'.$iTags.'</p>';
 	}
 }
 ?>
