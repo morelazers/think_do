31592b5ec00ff6aa49f125d1b8ebf12922e03984
Tom Nash <tommynosh@hotmail.com>
2013-02-05 21:32:37 +0000

    Update matchmaking_functions.php

--- matchmaking_functions.php
+++ matchmaking_functions.php
@@ -8,7 +8,10 @@
 
 function getUserInterests($u, $c)
 {
-
+      $ids = join(',',$u['interests']);  
+      $sql = "SELECT id FROM interests WHERE id IN ($ids)";
+      $result = mysql_query($sql, $c);
+      var_dump($result);
 }
 
 ?>
