2fff5fcc6dbb4038448857130dfb464ae853a78f
Tom Nash <tommynosh@hotmail.com>
2013-02-05 12:25:36 +0000

    Update list_ideas.php
    now displays the number of upvotes that a particular idea has

--- list_ideas.php
+++ list_ideas.php
@@ -14,20 +14,22 @@ include 'header.php'; ?>
 <?php include 'footer.php'; ?>
 
 <?php
-function outputIdeas(&$projects)
+function outputIdeas(&$ideas)
 {
 	echo '<table>';
 	//Create a table and output the project creator, created date and project name to table. The project name is linked to the project page
-	while($projectsArray = mysql_fetch_array($projects))
+	while($ideasArray = mysql_fetch_array($ideas))
 	{
-		$pName = $projectsArray['projectName'];
-		$createdBy = $projectsArray['createdBy'];
-		$dateCreated = $projectsArray['dateCreated'];
-		$pID = $projectsArray['projectID'];
+		$iName = $ideasArray['ideaName'];
+		$createdBy = $ideasArray['createdBy'];
+		$dateCreated = $ideasArray['dateCreated'];
+		$iID = $ideasArray['ideaID'];
+		$iVotes = $ideasArray['upVotes'];
 		echo '<tr>';
-		echo '<td><h2><a href="./view_ideas.php?pid='.$pID.'">'.$pName.'</a></h2></td>';
+		echo '<td><h2><a href="./view_ideas.php?pid='.$iID.'">'.$iName.'</a></h2></td>';
 		echo '<td>'.$createdBy.'</td>';
 		echo '<td>'.$dateCreated.'</td>';
+		echo '<td>'.$iVotes.'</td>';
 		echo '</tr>';
 	}
 	echo '</table><br>';
@@ -43,27 +45,20 @@ function getIdeas()
 		$startID = $_GET["page"] * 10;
 		//Add 9 to start ID to get final ID for page
 		$endID = $startID + 9;
-		//Query database for data for all projects to be displayed on this page
-		$projects = mysql_query("SELECT createdBy, projectName, dateCreated, projectID FROM project WHERE projectID BETWEEN " . $startID . " AND " . $endID);
-		$projectAmount = mysql_query("SELECT COUNT(*) FROM project");
-		//If query returns projects
-		if($projects!=null)
-		{
-			outputIdeas($projects);
-		}
 	}
 	else
 	{
 		//If no page parameter in URL begin from ID = 0
 		$startID = 0;
 		$endID = 9;
-		//Query database for data for all projects to be displayed on this page
-		$projects = mysql_query("SELECT createdBy, projectName, dateCreated, projectID FROM project WHERE projectID BETWEEN " . $startID . " AND " . $endID);
-		//If query returns projects
-		if($projects!=null)
-		{
-			outputIdeas($projects);
-		}
+	}
+	//Query database for data for all projects to be displayed on this page
+	$ideas = mysql_query("SELECT * FROM idea WHERE ideaID BETWEEN " . $startID . " AND " . $endID);
+	$ideaAmount = mysql_query("SELECT COUNT(*) FROM idea");
+	//If query returns projects
+	if($ideas != null)
+	{
+		outputIdeas($ideas);
 	}
 	
 }
