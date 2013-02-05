aa838002e7c75a1d785b8eec8afb20525edf9bb7
Tom Nash <tommynosh@hotmail.com>
2013-02-05 21:35:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include 'header.php'; ?>
 		<div class="post">
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
 			<?php
+			include 'matchmaking_functions.php';
 			if (isset($_SESSION['usr']))
 			{
 				getUserInterests($_SESSION['usr']);	
