b7c22458b1638c2c241faf276d28c79f6ae8f063
Tom Nash <tommynosh@hotmail.com>
2013-02-12 12:30:44 +0000

    Update matchmaking_functions.php

--- matchmaking_functions.php
+++ matchmaking_functions.php
@@ -16,13 +16,11 @@ function getUserInterests($u, $c)
 function getAllInterests($c)
 {
       $sql = mysql_real_escape_string("SELECT * FROM interests");
-      $result = mysql_query($sql, $c)
-      or die(mysql_error());
+      $result = mysql_query($sql, $c);
       echo mysql_error();
-      var_dump($result);
       while($row = mysql_fetch_array($result))
       {
-            $GLOBALS['interests'] = $row;
+            $GLOBALS['interests'][$row['id']] = $row['name'];
       }
       var_dump($GLOBALS['interests']);
 }
