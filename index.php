35440303c64c8111c3ea278c5060349b097eabfd
emeryn314 <nathanemery2@gmail.com>
2013-02-12 12:08:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,11 @@ include 'header.php'; ?>
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
 			<?php
 			include 'matchmaking_functions.php';
-			if (isset($_SESSION['usr']))
+			/*if (isset($_SESSION['usr']))
 			{
 				getUserInterests($_SESSION['usr']);	
 			}
+			*/
 			getAllInterests();
 			?>
 		</div>			
