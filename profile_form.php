4d1a13074d9c1f2df826100bd77ffbd256ef67ac
Tom Nash <tommynosh@hotmail.com>
2013-02-13 20:38:21 +0000

    Update profile_form.php

--- profile_form.php
+++ profile_form.php
@@ -47,7 +47,15 @@ if (isset($_SESSION['usr']))
     	{
     		if (profileInputIsComplete())
         	{   
-        		updateProfileInfo($con);
+        		/*
+        		 * Functionality needed to take the inputted interests and convert them to
+        		 * a string of comma-separated integers to be passed into the update function
+        		 */
+        		$aboutMe = mysql_real_escape_string($aboutMe);
+        		$interests = mysql_real_escape_string($interests);
+        		$skills = mysql_real_escape_string($skills);
+        		
+        		updateProfileInfo($con, $aboutMe, $interests, $skills);
         	}
     	}
 }
