08fc0200a8a0bb17cbbeafeb1159d88c91c88c13
Tom Nash <tommynosh@hotmail.com>
2013-02-14 09:00:43 +0000

    Update profile_form.php

--- profile_form.php
+++ profile_form.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['usr']))
         	if (passInputIsComplete())
         	{
 		
-			if (checkPass($con, $pass1, $currentUser))
+			if (checkPass($con, $oldPass, $currentUser))
 			{
 				changePass($con, $currentUser, $pass1);
 			}
