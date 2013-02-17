a5ef9730b9522ba260066fde485ed55905824d13
Tom Nash <tommynosh@hotmail.com>
2013-02-17 23:23:02 +0000

    Update view_ideas.php

--- view_ideas.php
+++ view_ideas.php
@@ -10,7 +10,7 @@
 
             include 'connect.php';
 
-            include 'mysql_functions.php'
+            include 'mysql_functions.php';
 
             $idea = getIdea();
 
@@ -23,21 +23,5 @@
 <?php 
 
 include 'footer.php'; 
-
-/**
- *  Function to output the data from the idea to the page
- *  @param idea $i assosciative array containing the fields fom thr idea table
- */
-function showIdea($i)
-{
-	//Output project information with appropriate markup
-	echo '<h2>'.$i['ideaName'].'</h2><br>';
-	echo '<h3>Idea Description:</h3><br>';
-	echo '<p>'.$i['description'].'</p><br>';
-	echo '<h3>Skills Needed:</h3><br>';
-	echo '<p>'.$i['skillsRequired'].'</p><br>';
-	echo '<h3>Idea Tags:</h3><br>';
-	echo '<p>'.$i['tags'].'</p>';
-}
 
 ?>
