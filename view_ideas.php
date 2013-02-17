dab7a965e152aab174899faa8d2037c059e0d502
Tom Nash <tommynosh@hotmail.com>
2013-02-17 23:03:07 +0000

    Update view_ideas.php

--- view_ideas.php
+++ view_ideas.php
@@ -1,41 +1,43 @@
 <?php
 /**
-	Author: Nathan Emery
+*	@author: Nathan Emery
 */
  include 'header.php'; ?>
     <div class="clear"></div>
         <div id="post-container">
         	<div class="post">
-            <?php getIdea(); ?>
+            <?php 
+
+            include 'connect.php';
+
+            include 'mysql_functions.php'
+
+            $idea = getIdea();
+
+            showIdea($idea);
+
+            ?>
             </div>
         </div>
     </div>
-<?php include 'footer.php'; ?>
+<?php 
 
-<?php
-function getIdea(){
-	//Connect to mysql
-	include 'connect.php';
-	//Check for 'pid' parameter in URL
-	if(array_key_exists("pid", $_GET))
-	{
-		$ideaID = $_GET["pid"];
-		$idea = mysql_query("SELECT * FROM idea WHERE ideaID =" . $ideaID);
-		//Get project data for the project from the database
-		$ideaArray = mysql_fetch_array($idea);
-		//Assign query results to variables
-		$iName = $ideaArray['projectName'];
-		$iDesc = $ideaArray['description'];
-		$iSkills = $ideaArray['skillsRequired'];
-		$iTags = $ideaArray['tags'];
-		//Output project information with appropriate markup
-		echo '<h2>'.$iName.'</h2><br>';
-		echo '<h3>Idea Description:</h3><br>';
-		echo '<p>'.$iDesc.'</p><br>';
-		echo '<h3>Skills Needed:</h3><br>';
-		echo '<p>'.$iSkills.'</p><br>';
-		echo '<h3>Idea Tags:</h3><br>';
-		echo '<p>'.$iTags.'</p>';
-	}
+include 'footer.php'; 
+
+/**
+ *  Function to output the data from the idea to the page
+ *  @param idea $i assosciative array containing the fields fom thr idea table
+ */
+function showIdea($i)
+{
+	//Output project information with appropriate markup
+	echo '<h2>'.$i['ideaName'].'</h2><br>';
+	echo '<h3>Idea Description:</h3><br>';
+	echo '<p>'.$i['description'].'</p><br>';
+	echo '<h3>Skills Needed:</h3><br>';
+	echo '<p>'.$i['skillsRequired'].'</p><br>';
+	echo '<h3>Idea Tags:</h3><br>';
+	echo '<p>'.$i['tags'].'</p>';
 }
+
 ?>
