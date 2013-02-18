b9f89dc23ba0e13e559fe287ac557ddc00a0d101
Tom Nash <tommynosh@hotmail.com>
2013-02-18 03:32:01 +0000

    Update mysql_functions.php

--- mysql_functions.php
+++ mysql_functions.php
@@ -77,13 +77,7 @@
 	}
 }
 
-function showCommentForm()
-{
-	echo '<form method="post" action="'; echo $PHP_SELF; echo '">
-    <label for="comment">Post a comment:</label><br>
-    <input type="text" name="content" id="contentInput" width="100%" value="">
-    <br><input type="submit" name="submit" value="Submit"></form>';
-}
+
 /**
  *  MySQL function to verify that the entered password is correct during a login attempt
  *	@param MySQLConnection $c Connection to MySQL database, necessary to perform queries
