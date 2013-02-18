db200b9ad387bf7924407201a6b4932302151c83
Tom Nash <tommynosh@hotmail.com>
2013-02-18 03:23:33 +0000

    Update view_ideas.php

--- view_ideas.php
+++ view_ideas.php
@@ -12,12 +12,16 @@
 
             include 'mysql_functions.php';
 
+            if(isset($_POST['submit'])){
+                postComment();
+            }
+
             $idea = getIdea();
 
             showIdea($idea);
             echo '<br><hr>';
             getComments();
-            echo '<br><hr>';
+
             showCommentForm();
 
             ?>
@@ -36,5 +40,13 @@ function showCommentForm()
     <label for="comment">Post a comment:</label><br>
     <input type="text" name="content" id="contentInput" value="">
     <br><input type="submit" name="submit" value="Submit"></form>';
+}
+
+function postComment()
+{
+    $u = $_SESSION['usr'];
+    $n = $u['username'];
+    $now = date("y-m-d H:i:s");
+    mysql_query($con, "INSERT INTO comments (ideaID, parentID, username, content, datePosted, upVotes) VALUES (".$_GET["pid"].", 0,".$n.",".$_POST['content'].",".$now.",0");
 }
 ?>
