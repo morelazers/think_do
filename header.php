c7d1c787056cd17ce7413a906401cae0ce3e8c19
mingkitty <mingkitwongg@gmail.com>
2013-02-18 01:06:33 +0000

    Update header.php

--- header.php
+++ header.php
@@ -13,7 +13,6 @@ getAllInterests($con);
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link rel="stylesheet" href="webfonts/stylesheet.css" type="text/css" charset="utf-8" />
     <link rel="SHORTCUT ICON" href="favicon.ico" type="image/x-icon" /> 
-    <link rel="stylesheet" type="text/css" href="css/autocomplete.css" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
 	<script language="javascript" type="text/javascript">
