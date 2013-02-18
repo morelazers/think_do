5c798506b8b0e34b0a23f1805f977876b691f6ca
Tom Nash <tommynosh@hotmail.com>
2013-02-18 06:47:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ include 'header.php'; ?>
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
 			<?php
 			include 'connect.php';
-			//include 'functions_think.php';
-			/*if (!isset($GLOBALS['interests']))
+			include 'functions_think.php';
+			if (!isset($GLOBALS['interests']))
 			{
 				getAllInterests($con);
-			}*/
+			}
 			?>
 		</div>			
 	</div>	
