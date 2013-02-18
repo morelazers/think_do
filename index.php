97d5db4c766449c93cd6ca90d1c2eae35ca130ad
emeryn314 <nathanemery2@gmail.com>
2013-02-18 06:44:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'header.php'; ?>
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
 			<?php
 			include 'connect.php';
-			include 'functions_think.php';
+			//include 'functions_think.php';
 			/*if (!isset($GLOBALS['interests']))
 			{
 				getAllInterests($con);
