bbbc34fff7a9e196ddd9ea2578bdaa9f07c47a3f
Tom Nash <tommynosh@hotmail.com>
2013-02-18 05:00:38 +0000

    Update modify_idea_form.php

--- modify_idea_form.php
+++ modify_idea_form.php
@@ -1,7 +1,5 @@
 <?php session_start();
 
-include 'header.php';
-
 include 'connect.php';
 
 include 'mysql_functions.php';
@@ -9,7 +7,5 @@ include 'mysql_functions.php';
 $_SESSION['modIdea'] = getIdeaData();
 
 include 'idea_form.php';
-
-include 'footer.php';
 
 ?>
