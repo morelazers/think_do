5655fc19bf7d6a993739fe471eae507c09147e60
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-02-18 00:31:15 +0000

    more css

--- dev/footer.php
+++ dev/footer.php
@@ -3,13 +3,20 @@
 		Author: Mingkitty Wong
 	*/
 ?>
-<div id="footer">
-    <div id="links">
+
+
+</div>
+</div><!--END DIV MAIN-->
+
+<div class="footer">
+
+<div id="links">
         <o><a href="about.php">About</a></p>
     </div>
     <div id="copyright">
 	<p> Copyright &copy; Think.Do <script type="text/javascript"> document.write(dateObject.getFullYear());</script></p>
 	</div>
+
 </div>
 </body>
-</html>   
+</html>  
--- dev/idea_form.php
+++ dev/idea_form.php
@@ -56,7 +56,18 @@ else
 
     function showForm() 
     {
-        echo '<form method="post" action="'; echo $PHP_SELF; echo '">
+        echo '
+<div class="main">
+	<div class="sidebar">
+	
+	<h1>TOP TIPS</h1>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id nisl lectus. Donec lacinia justo ut risus viverra mattis. Aliquam sed iaculis lorem. Donec sit amet enim at massa iaculis auctor. Suspendisse posuere iaculis dictum. Nulla in sapien sed diam sodales vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam dictum mi ac tortor mattis tristique. Cras varius tempor odio ut malesuada. Fusce at ligula velit, ac mollis eros. Quisque in nulla in leo dignissim pulvinar ut quis lacus. Maecenas ornare sollicitudin libero, id molestie mauris iaculis ut. Donec sit amet nulla eu ligula tristique congue. Etiam justo nisl, pharetra id scelerisque in, elementum et magna. Suspendisse potenti.
+	
+	</div>
+	
+	
+	<div class="mainRight">
+	<form method="post" action="'; echo $PHP_SELF; echo '">
         <label for="idea_title">Name your idea:</label><br>
         <input type="text" name="ideaName" id="idea_title" value="';
     echo $_POST["ideaName"];
