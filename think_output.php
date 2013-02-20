4c108dd9680b2afdd5b41da8aae3b9854cfb8ee6
Tom Nash <tommynosh@hotmail.com>
2013-02-20 19:28:51 +0000

    Update think_output.php

--- think_output.php
+++ think_output.php
@@ -2,5 +2,14 @@
 
 include 'header.php';
 include 'functions_think.php';
+include 'connect.php';
 
 ?>
+<div class="clear"></div>
+        <div id="post-container">
+          <div class="post">
+                <?php think($con); ?>
+            </div>
+        </div>
+    </div>
+<?php include 'footer.php'; ?>
