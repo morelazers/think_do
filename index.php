37ca5a4b08a614e0eda64668436e837545b55ebf
Tom Nash <tommynosh@hotmail.com>
2013-02-20 19:29:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,13 @@ include 'functions_think.php'; ?>
 	<div id="post-container">
 		<div class="post">
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
-			<a href="think_output.php">Think!</a><br>
+			<?php
+			if(isset($_SESSION['usr']))
+			{
+				echo '<a href="think_output.php">Think!</a><br>';	
+			}
+			?>
+			
 		</div>			
 	</div>	
 </div>
