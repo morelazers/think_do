1f1baac1e84cb19788140be782c8b3a342ae50b4
Tom Nash <tommynosh@hotmail.com>
2013-02-20 19:26:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 	/**
 		Author: Mingkit Wong
 	*/
-include 'header.php'; ?>
+include 'header.php';
+include 'functions_think.php'; ?>
 	<div class="clear"></div>
 	<div id="post-container">
 		<div class="post">
 			<p>Thinkdo is a website designed to help people get help with their ideas or projects. It is open to all, and shows no discrimination.</p>
+			<a href="think_output.php">Think!</a><br>
 		</div>			
 	</div>	
 </div>
