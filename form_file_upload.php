2df74a1f6df1550d8b666e4ed0e9b7b6c6ad954c
Tom Nash <tommynosh@hotmail.com>
2013-02-21 21:58:33 +0000

    my github's being a jerk

---
+++ form_file_upload.php
@@ -0,0 +1,52 @@
+<?php
+
+showUploadForm();
+
+$allowedExts = array("jpg", "jpeg", "gif", "png");
+$extension = end(explode(".", $_FILES["file"]["name"]));
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts))
+{
+	if ($_FILES["file"]["error"] > 0)
+	{
+		echo "Error: " . $_FILES["file"]["error"] . "<br>";
+	}
+	else
+	{
+		echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+		echo "Type: " . $_FILES["file"]["type"] . "<br>";
+		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+		echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+		if (file_exists("upload/" . $_FILES["file"]["name"]))
+      	{
+      		echo $_FILES["file"]["name"] . " already exists. ";
+     	}
+    	else
+      	{
+		    move_uploaded_file($_FILES["file"]["tmp_name"],
+		    "upload/" . $_FILES["file"]["name"]);
+		    echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      	}
+	}
+}
+else
+{
+	echo "Invalid file";
+}
+
+function showUploadForm()
+{
+	echo '<form action="' echo $PHP_SELF; '" method="post"
+	enctype="multipart/form-data">
+	<label for="file">Filename:</label>
+	<input type="file" name="file" id="file"><br>
+	<input type="submit" name="submit" value="Submit">
+	</form>';
+}
+
+?>
--- index.php
+++ index.php
@@ -20,6 +20,7 @@ include 'functions_idea.php'; ?>
 				if(isset($u['interests']))
 				{
 					echo '<a href="think_output.php">Think!</a><br>';
+					echo '<a href="upload_file.php">Upload!</a><br>';
 				}
 				else
 				{
