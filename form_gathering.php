677361bd291ca3b3ab9b83ff9f7348b84a06cbb6
Tom Nash <tommynosh@hotmail.com>
2013-02-22 18:02:02 +0000

    kapeesh

---
+++ form_gathering.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'functions_input.php';
+
+showGatheringForm();
+
+if(isset($_POST['submit']))
+{
+	if(inputIsComplete())
+	{
+		$gDesc = mysql_real_escape_string($_POST['gatheringDescription']);
+		$gLoc = mysql_real_escape_string($_POST['gatheringLocation']);
+		$date = $_POST['gatheringDate'];
+		$time = $_POST['gatheringTime'];
+		$sql = "INSERT INTO gatherings (description, location, gdate, gtime) VALUES ('".$gDesc."', '".$gLoc."', '".$date."', '".$time."')";
+		mysql_query($sql) or die(mysql_error());
+	}
+}
+
+function showGatheringForm()
+{
+	echo '<form method="post" action="'; 
+    echo $PHP_SELF; 
+    echo '">
+    <label for="gatheringDescription">What should the gathering accomplish?</label><br>
+    <input type="text" name="gatheringDescription" id="gatheringDescription" value=""><br>
+	<label for="gatheringLocation">Where will it be?</label><br>
+    <input type="text" name="gatheringLocation" id="gatheringLocation" value=""><br>
+	<label for="gatheringDate">On which date?</label><br>
+    <input type="date" name="gatheringDate" id="gatheringDate" value=""><br>
+    <label for="gatheringTime">At what time?</label><br>
+    <input type="time" name="gatheringTime" id="gatheringTime" value=""><br>
+    <input type="submit" name="submitAboutMe" value="Update">
+    </form>';
+}
+
+?>
