5da416c14544bc4641b6f7fcd3645e9db130a4ab
Tom Nash <tommynosh@hotmail.com>
2013-02-23 15:55:03 +0000

    should work now

--- view_task.php
+++ view_task.php
@@ -1,31 +1,46 @@
 <?php
 
-	/*
-	 *	We don't even really need to run an SQL query here if we use some JQuery
-	 *	to have a drop down when the task is clicked
-	 */
+	include 'header.php';
 
-	if(array_key_exists("pid", $_GET))
-	{
-		$taskID = $_GET["pid"];
-		var_dump($taskID);
-		$sql = "SELECT * FROM tasks WHERE taskID =" . $taskID;
-		$res = mysql_query($sql) or die(mysql_error());
-		//Get project data for the project from the database
-		$task = mysql_fetch_array($res);
-		var_dump($task);
-		
-		if ($task == null)
-    	{
-       		//header('Location: error_page.php');
-       		echo 'help';
-	    }
-		else
-		{
-			echo '<h2>'.$task['taskName'].'</h2><br>';
-			echo '<h3>Task Description:</h3><br>';
-			echo '<p>'.$task['taskDescription'].'</p><br>';
-		}
-	}
+	?>
+
+	<div class="clear"></div>
+        <div id="post-container">
+    		<div class="post">
+    			<?php
+					/*
+					 *	We don't even really need to run an SQL query here if we use some JQuery
+					 *	to have a drop down when the task is clicked
+					 */
+
+					if(array_key_exists("pid", $_GET))
+					{
+						$taskID = $_GET["pid"];
+						var_dump($taskID);
+						$sql = "SELECT * FROM tasks WHERE taskID =" . $taskID;
+						$res = mysql_query($sql) or die(mysql_error());
+						//Get project data for the project from the database
+						$task = mysql_fetch_array($res);
+						var_dump($task);
+						
+						if ($task == null)
+				    	{
+				       		//header('Location: error_page.php');
+				       		echo 'help';
+					    }
+						else
+						{
+							echo '<h2>'.$task['taskName'].'</h2><br>';
+							echo '<h3>Task Description:</h3><br>';
+							echo '<p>'.$task['taskDescription'].'</p><br>';
+						}
+					}
+
+				?>
+            </div>
+        </div>
+    </div>
+    <?php
+	include 'footer.php';
 
 ?>
