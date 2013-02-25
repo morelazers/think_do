e728d93b11051ba8e96e99f7223f44cea1ffd7b1
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-02-25 14:16:33 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,18 +8,9 @@ include 'functions_idea.php'; ?>
 	<div id="post-container">
 	<div class="sidebar">
 	
-	<h1>Homepage Sidebar</h1>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id nisl lectus. 
-	Donec lacinia justo ut risus viverra mattis. Aliquam sed iaculis lorem. 
-	Donec sit amet enim at massa iaculis auctor. Suspendisse posuere iaculis dictum. 
-	Nulla in sapien sed diam sodales vehicula. 
-	Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-	Etiam dictum mi ac tortor mattis tristique. Cras varius tempor odio ut malesuada. 
-	Fusce at ligula velit, ac mollis eros. Quisque in nulla in leo dignissim pulvinar ut quis lacus. 
-	Maecenas ornare sollicitudin libero, id molestie mauris iaculis ut. 
-	Donec sit amet nulla eu ligula tristique congue. 
-	Etiam justo nisl, pharetra id scelerisque in, elementum et magna. 
-	Suspendisse potenti.
+	<h1>Think.Do!</h1>
+	Click think to find ideas we've matched with your interests!
+	
 	
 	</div>
 	
@@ -46,6 +37,7 @@ include 'functions_idea.php'; ?>
 					<br></p>";
 				}
 			}
+			echo '<h2>Top Ten Ideas</h2>'
 			getHomepageIdeas($con);
 			?>
 			
