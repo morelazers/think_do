11fe01ce58e9e1b49f2d086155827d6dded2fc82
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-02-25 14:07:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,24 @@ include 'header.php';
 include 'functions_idea.php'; ?>
 	<div class="clear"></div>
 	<div id="post-container">
-		<div class="post">
+	<div class="sidebar">
+	
+	<h1>Homepage Sidebar</h1>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id nisl lectus. 
+	Donec lacinia justo ut risus viverra mattis. Aliquam sed iaculis lorem. 
+	Donec sit amet enim at massa iaculis auctor. Suspendisse posuere iaculis dictum. 
+	Nulla in sapien sed diam sodales vehicula. 
+	Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+	Etiam dictum mi ac tortor mattis tristique. Cras varius tempor odio ut malesuada. 
+	Fusce at ligula velit, ac mollis eros. Quisque in nulla in leo dignissim pulvinar ut quis lacus. 
+	Maecenas ornare sollicitudin libero, id molestie mauris iaculis ut. 
+	Donec sit amet nulla eu ligula tristique congue. 
+	Etiam justo nisl, pharetra id scelerisque in, elementum et magna. 
+	Suspendisse potenti.
+	
+	</div>
+	
+		<div class="mainRight">
 			<p>Welcome to think.do! We're a site dedicated to the future, but we need your help!
 			<br>
 			If you have an idea - be it big or small - we want you to share it here!
