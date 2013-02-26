60066d876886cf7b9d9cbb0ec1839a93316daaec
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-02-26 13:16:46 +0000

    Update idea_form.php

--- idea_form.php
+++ idea_form.php
@@ -13,17 +13,12 @@ if (isset($_SESSION['usr']))
 echo ' 	<div class="sidebar">
 	
 	<h1>TOP TIPS</h1>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id nisl lectus. 
-	Donec lacinia justo ut risus viverra mattis. Aliquam sed iaculis lorem. 
-	Donec sit amet enim at massa iaculis auctor. Suspendisse posuere iaculis dictum. 
-	Nulla in sapien sed diam sodales vehicula. 
-	Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-	Etiam dictum mi ac tortor mattis tristique. Cras varius tempor odio ut malesuada. 
-	Fusce at ligula velit, ac mollis eros. Quisque in nulla in leo dignissim pulvinar ut quis lacus. 
-	Maecenas ornare sollicitudin libero, id molestie mauris iaculis ut. 
-	Donec sit amet nulla eu ligula tristique congue. 
-	Etiam justo nisl, pharetra id scelerisque in, elementum et magna. 
-	Suspendisse potenti.
+	<b>Title</b></br>
+	The title is the first thing people will see! Your idea could be revolutionary, make the title exciting!</br></br>
+	<b>Description</b></br>
+	The title is the first thing people will see! Your idea could be revolutionary, make the title exciting!</br></br>
+	<b>Title</b></br>
+	The title is the first thing people will see! Your idea could be revolutionary, make the title exciting!</br></br>
 	
 	</div>
 	
