2264f230ee3038d66320771528d70f5dec208e0a
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-02-26 14:22:28 +0000

    temp remove footer

--- footer.php
+++ footer.php
@@ -3,15 +3,6 @@
 		Author: Mingkitty Wong
 	*/
 ?>
-<div class="footer">
 
-<div id="links">
-        <o><a href="about.php">About</a></p>
-    </div>
-    <div id="copyright">
-	<p> Copyright &copy; Think.Do <script type="text/javascript"> document.write(dateObject.getFullYear());</script></p>
-	</div>
-
-</div>
 </body>
 </html>   
