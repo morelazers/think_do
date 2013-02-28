8d2445af43e9217380b09c00890afa3ab59f522a
Tom Nash <tommynosh@hotmail.com>
2013-02-28 15:00:55 +0000

    should work

--- functions_gatherings.php
+++ functions_gatherings.php
@@ -4,15 +4,15 @@ function getIdeaGatherings($i)
 {
 	$sql = "SELECT * FROM gatherings WHERE forIdea = ".$i['ideaID'];
 	$res = mysql_query($sql);
-	$gaths = mysql_fetch_array($res);
-	return $gaths;
+	//$gaths = mysql_fetch_array($res);
+	return $res;
 }
 
-function displayGatherings($g)
+function displayGatherings(&$g)
 {
 	while ($curGath = mysql_fetch_array($g))
 	{
-		var_dump($curGath);
+		//var_dump($curGath);
 		echo '<tr>';
 		echo '<td><h2><a href="./view_gathering.php?pid='.$curGath['gathID'].'">'.$curGath['gathLocation'].'</a></h2></td>';
 		echo '<td>'.$curGath['gathDate'].'</td>';
