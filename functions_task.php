ef67d4e952ffbf811f94143c3b89d80149271d8a
Tom Nash <tommynosh@hotmail.com>
2013-03-03 16:37:58 +0000

    commit

--- functions_task.php
+++ functions_task.php
@@ -4,8 +4,8 @@
 function createTask($i, $n, $d, $dline, $u, $c)
 {
 	$now = date("Y-m-d H:i:s");
-	$sql = "INSERT INTO tasks (ideaID, taskName, username, taskDescription, complete, dateCreated)
-	VALUES ('".$i['ideaID']."', '".$n."', '".$u['username']."', '".$d."', '0', '".$dline."', '".$now."')";
+	$sql = "INSERT INTO tasks (ideaID, taskName, username, taskDescription, complete, deadline, dateCreated)
+	VALUES ('".$i['ideaID']."', '".$n."', '".$u['username']."', '".$d."', 0, '".$dline."', '".$now."')";
 	mysql_query($sql, $c)
 	or die(mysql_error());
 }
