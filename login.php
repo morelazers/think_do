2d9685c81e8378510ac2cc623a71321d681c3a8c
emeryn314 <nathanemery2@gmail.com>
2013-03-06 15:43:55 +0000

    Update login.php

--- login.php
+++ login.php
@@ -4,9 +4,9 @@
 	include 'header.php'; ?>
         <div class="clear"></div>
         <div id="post-container">
-    		<div class="post">
+    		<div class="post"><div class="smallForm">
 		        <?php include 'login_form.php'; ?>
-            </div>
+            </div></div>
         </div>
     </div>
 <?php include 'footer.php'; ?>
