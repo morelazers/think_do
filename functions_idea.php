20018dd021a9edb3973950cf45027f6c29fa5e3e
Tom Nash <tommynosh@hotmail.com>
2013-03-11 13:49:00 +0000

    debug dumps

--- functions_idea.php
+++ functions_idea.php
@@ -145,7 +145,9 @@ function userMemberStatus($i, $u, $c){
 	$sql = "SELECT * FROM idea WHERE ideaID = ".$i['ideaID'];
 	$queryResults = mysql_query($sql, $c);
 	$resultString = mysql_fetch_array($queryResults);
+	var_dump($resultString);
 	$resultsArray = explode(",", $resultsString['helpers']);
+	var_dump($resultsArray);
 	if(in_array($u['userID'], $resultsArray)){
 		return 1;
 	}
@@ -154,7 +156,9 @@ function userMemberStatus($i, $u, $c){
 	$sql = "SELECT * FROM idea WHERE ideaID = ".$i['ideaID'];
 	$queryResults = mysql_query($sql, $c);
 	$resultString = mysql_fetch_array($queryResults);
+	var_dump($resultString);
 	$resultsArray = explode(",", $resultsString['moderators']);
+	var_dump($resultsArray);
 	if(in_array($u['userID'], $resultsArray)){
 		return 2;
 	}
