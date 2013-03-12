a87e116cf06c3fb5ad1a10640eba506102d56531
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-03-12 12:54:38 +0000

    more sidebars yeeeee

--- modify_profile.php
+++ modify_profile.php
@@ -2,9 +2,38 @@
     <div class="clear"></div>
         <div id="post-container">
           <div class="post">
+          	<div class="sidebar">	
+	<h1>think.do</h1>
+	Welcome to think.do! We're a site dedicated to the future, but we need your help!
+	<br><br>
+	If you have an idea - be it big or small - we want you to share it here!
+	<br><br>
+	We also believe that what goes around comes around, so if you see an idea that you like and think you can help with, give it a shot! You never know what you might achieve together!</p>
+	<?php
+	//include 'functions_think.php';
+	if(isset($_SESSION['usr']))
+	{
+		$u = $_SESSION['usr'];
+		if(isset($u['interests']))
+		{
+			echo '<a href="think_output.php"><img src="images/think.png"/></a><br>';
+		}
+		else
+		{
+			echo "<p>We've noticed you haven't filled out any interests in your profile yet!
+			<br>
+			To get the best out of think.do we recommend that you edit your profile to include a few interests!
+			<br></p>";
+		}
+	}
+	?>
+	</div>
+	
+		<div class="mainRight">
             <?php include 'profile_form.php'; ?>
             </div>
         </div>
     </div>
+</div>
 <?php include 'footer.php'; ?>
 
