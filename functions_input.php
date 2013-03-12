53dd0a644b97c2d3a1a37ec6d33baf44d8a48cf6
Tom Nash <tommynosh@hotmail.com>
2013-03-12 12:52:45 +0000

    "

--- functions_input.php
+++ functions_input.php
@@ -58,10 +58,12 @@ function getInterestIDs($i, $c)
             //$val = '"' .$val. '"';
             //var_dump($val);
             //if(in_array($val, $GLOBALS['interests']))
-            foreach($GLOBALS['interests'] as $intVal)
+            $globalInterestCount = count($GLOBALS['interests']);
+            $index = 0;
+            for($index; $index == $globalInterestCount; $index++)
             {
-                echo $intVal.'<br>';
-                if(strcmp(strtolower($val), strtolower($intVal)) == 0)
+                //echo $intVal.'<br>';
+                if(strcmp(strtolower($val), strtolower($GLOBALS['interests'][$index])) == 0)
                 {
                     $IDArray[] = $val;
                     echo 'found a value in the database<br>';
