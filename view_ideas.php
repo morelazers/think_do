65738b193bacb4e91784e4f957e6f6e6917393e7
Mingkit Wong <mingkitwongg@gmail.com>
2013-03-15 17:44:05 +0000

    "
    cleaned up indentation a little

--- view_ideas.php
+++ view_ideas.php
@@ -11,138 +11,138 @@
 	</script>
     <div class="clear"></div>
         <div id="post-container">
-         <div class="post">
-        <div class="sidebar">   
-    <h1>think.do</h1>
-    Welcome to think.do! We're a site dedicated to the future, but we need your help!
-    <br><br>
-    If you have an idea - be it big or small - we want you to share it here!
-    <br><br>
-    We also believe that what goes around comes around, so if you see an idea that you like and think you can help with, give it a shot! You never know what you might achieve together!</p>
-    <?php
-    //include 'functions_think.php';
-    if(isset($_SESSION['usr']))
-    {
-        $u = $_SESSION['usr'];
-        if(isset($u['interests']))
-        {
-            echo '<a href="think_output.php"><img src="images/think.png"/></a><br>';
-        }
-        else
-        {
-            echo "<p>We've noticed you haven't filled out any interests in your profile yet!
-            <br>
-            To get the best out of think.do we recommend that you edit your profile to include a few interests!
-            <br></p>";
-        }
-    }
-    ?>
-    </div>
+            <div class="post">
+                <div class="sidebar">   
+                    <h1>think.do</h1>
+                    Welcome to think.do! We're a site dedicated to the future, but we need your help!
+                    <br><br>
+                    If you have an idea - be it big or small - we want you to share it here!
+                    <br><br>
+                    We also believe that what goes around comes around, so if you see an idea that you like and think you can help with, give it a shot! You never know what you might achieve together!</p>
+                    <?php
+                    //include 'functions_think.php';
+                    if(isset($_SESSION['usr']))
+                    {
+                        $u = $_SESSION['usr'];
+                        if(isset($u['interests']))
+                        {
+                            echo '<a href="think_output.php"><img src="images/think.png"/></a><br>';
+                        }
+                        else
+                        {
+                            echo "<p>We've noticed you haven't filled out any interests in your profile yet!
+                            <br>
+                            To get the best out of think.do we recommend that you edit your profile to include a few interests!
+                            <br></p>";
+                        }
+                    }
+                    ?>
+                </div>
     
-        <div class="mainRight">
-            <?php 
+                <div class="mainRight">
+                    <?php 
 
-            include 'connect.php';
+                    include 'connect.php';
 
-            include 'functions_idea.php';
-            include 'functions_comment.php';
-            include 'functions_task.php';
-            include 'functions_input.php';
-            include 'functions_gatherings.php';
-			$idea = getIdea();
-            if(isset($_POST['submitComment']))
-            {
-                postComment($parent);
-            }
-            elseif(isset($_POST['upvote'])){
-                
-                incrementIdeaUpvotes($idea,$_SESSION['usr'],$con);
-            }
-            elseif(isset($_POST['joinTeam'])){
-                joinIdeaTeam($idea, $_SESSION['usr'], $con);
-                $idea = getIdea();
-            }
+                    include 'functions_idea.php';
+                    include 'functions_comment.php';
+                    include 'functions_task.php';
+                    include 'functions_input.php';
+                    include 'functions_gatherings.php';
+        			$idea = getIdea();
+                    if(isset($_POST['submitComment']))
+                    {
+                        postComment($parent);
+                    }
+                    elseif(isset($_POST['upvote'])){
+                        
+                        incrementIdeaUpvotes($idea,$_SESSION['usr'],$con);
+                    }
+                    elseif(isset($_POST['joinTeam'])){
+                        joinIdeaTeam($idea, $_SESSION['usr'], $con);
+                        $idea = getIdea();
+                    }
 
-            
+                    
 
-            echo '
-            <div id="tabs">
-                <ul>
-                    <li><a href="#tabs-1">Description</a></li>
-                    <li><a href="#tabs-2">Todo List</a></li>
-                    <li><a href="#tabs-3">Gatherings</a></li>
-                </ul>
-                <div id="tabs-1">
-            ';
-            
-            if(isset($_SESSION['usr'])){
-                if(userHasVoted($idea, $_SESSION['usr'])==false){
-                    echo '<div style="float:right;"><form method="post" action="'; 
-                    echo $PHP_SELF; 
-                    echo '">
-                    <input type="submit" name="upvote" value="Upvote this idea">
-                    </form></div>';
-                }
-                else{
-                    echo "<p class='upVoted'>You've already upvoted this idea!</p>";
-                }
-                $ideaMember = userMemberStatus($idea, $_SESSION['usr'], $con);
-                if($ideaMember == 0){
-                    echo '<form method="post" action="'; 
-                    echo $PHP_SELF; 
-                    echo '">
-                    <input type="submit" name="joinTeam" value="Help Out!">
-                    </form>';
-                }
-                elseif($ideaMember == 1){
-                    echo "<p class='helperMsg'>You are helping this idea!</p>";
-                }
-                else{
-                    echo "<p class='modMsg'>You are an idea moderator</p>";
-                }
-            }
-            showIdea($idea);
-            echo '<br><hr>';
-            getComments($con);
-            if(isset($_SESSION['usr']))
-            {
-                showCommentForm();
-            }
-            else
-            {
-                echo "<h2>Oops!</h2></br>
-                    You must first <a href='login.php'>login</a> or <a href='register.php'>register</a> before you can post a comment!
-                   <br>
-                   But don't worry, it will take you less than a minute!
-                   <br>";
-            }
-            echo'
+                    echo '
+                    <div id="tabs">
+                        <ul>
+                            <li><a href="#tabs-1">Description</a></li>
+                            <li><a href="#tabs-2">Todo List</a></li>
+                            <li><a href="#tabs-3">Gatherings</a></li>
+                        </ul>
+                        <div id="tabs-1">
+                    ';
+                    
+                    if(isset($_SESSION['usr'])){
+                        if(userHasVoted($idea, $_SESSION['usr'])==false){
+                            echo '<div style="float:right;"><form method="post" action="'; 
+                            echo $PHP_SELF; 
+                            echo '">
+                            <input type="submit" name="upvote" value="Upvote this idea">
+                            </form></div>';
+                        }
+                        else{
+                            echo "<p class='upVoted'>You've already upvoted this idea!</p>";
+                        }
+                        $ideaMember = userMemberStatus($idea, $_SESSION['usr'], $con);
+                        if($ideaMember == 0){
+                            echo '<form method="post" action="'; 
+                            echo $PHP_SELF; 
+                            echo '">
+                            <input type="submit" name="joinTeam" value="Help Out!">
+                            </form>';
+                        }
+                        elseif($ideaMember == 1){
+                            echo "<p class='helperMsg'>You are helping this idea!</p>";
+                        }
+                        else{
+                            echo "<p class='modMsg'>You are an idea moderator</p>";
+                        }
+                    }
+                    showIdea($idea);
+                    echo '<br><hr>';
+                    getComments($con);
+                    if(isset($_SESSION['usr']))
+                    {
+                        showCommentForm();
+                    }
+                    else
+                    {
+                        echo "<h2>Oops!</h2></br>
+                            You must first <a href='login.php'>login</a> or <a href='register.php'>register</a> before you can post a comment!
+                           <br>
+                           But don't worry, it will take you less than a minute!
+                           <br>";
+                    }
+                    echo'
+                        </div>
+                        <div id="tabs-2">
+                    ';
+                        include 'todo_list.php';
+                        if(currentUserIsIdeaMod($idea))
+                        {
+                            include 'task_form.php';
+                        }
+                    echo '
+                        </div>
+                        <div id="tabs-3">
+                    ';
+                        include 'list_gatherings.php';
+                        if(currentUserIsIdeaMod($idea))
+                        {
+                            /* JQuery needed here I think, or at the top of the form file */
+                            include 'form_gathering.php';
+                        }
+                    echo '
+                        </div>
+                    </div>
+                    ';
+                    ?>
                 </div>
-                <div id="tabs-2">
-            ';
-                include 'todo_list.php';
-                if(currentUserIsIdeaMod($idea))
-                {
-                    include 'task_form.php';
-                }
-            echo '
-                </div>
-                <div id="tabs-3">
-            ';
-                include 'list_gatherings.php';
-                if(currentUserIsIdeaMod($idea))
-                {
-                    /* JQuery needed here I think, or at the top of the form file */
-                    include 'form_gathering.php';
-                }
-            echo '
-                </div>
-            </div>
-            ';
-            ?>
             </div>
         </div>
-    </div>
 <?php 
 
 include 'footer.php'; 
