5839b64253bbe10730cd613f04cb5a93c9bd29dc
Tom Nash <tommynosh@hotmail.com>
2013-03-19 17:39:27 +0000

    my head is full of

--- functions_idea.php
+++ functions_idea.php
@@ -132,13 +132,8 @@ function decrementIdeaUpvotes($i, &$u, $c)
 	$sql = "SELECT ideasVotedFor FROM user WHERE userID = ".$u['userID'];
 	$res = mysql_query($sql) or die(mysql_error());
 	$ideasString = mysql_fetch_array($res);
-
-	var_dump($ideasString);
-
 	$ideasArray = explode(',', $ideasString['ideasVotedFor']);
 	$count = 0;
-
-	var_dump($ideasArray);
 
 	var_dump($i['ideaID']);
 
