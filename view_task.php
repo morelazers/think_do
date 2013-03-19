f887c0b6b8b10dfe3293e29848b1b1e04e60b010
Tom Nash <tommynosh@hotmail.com>
2013-03-19 13:25:58 +0000

    trying to add a sidebar

--- view_task.php
+++ view_task.php
@@ -4,73 +4,75 @@ include 'header.php';
 include 'functions_task.php';
 include 'functions_user.php';
 
-?>
+if(array_key_exists("pid", $_GET))
+{
+	$taskID = $_GET["pid"];
 
-<div class="clear"></div>
+	if(isset($_POST['doTask']))
+	{
+		doTask($taskID, $_SESSION['usr']);
+	}
+	else if(isset($_POST['undoTask']))
+	{
+		undoTask($taskID, $_SESSION['usr']);
+	}
+	else if(isset($_POST['markAsComplete']))
+	{
+		markTaskAsComplete($taskID, $_SESSION['usr']);
+	}
+	else if(isset($_POST['notComplete']))
+	{
+		markTaskAsNotComplete($taskID);
+	}
+	$_SESSION['usr'] = getUserData($con, $_SESSION['usr']['username']);
+
+
+
+echo 
+'<div class="clear"></div>
     <div id="post-container">
 		<div class="post">
-			<?php
+			<div class="sidebar">';
 
-				if(array_key_exists("pid", $_GET))
+				if($ideaMember == 0)
 				{
-					$taskID = $_GET["pid"];
-
-					if(isset($_POST['doTask']))
-					{
-						doTask($taskID, $_SESSION['usr']);
-					}
-					else if(isset($_POST['undoTask']))
-					{
-						undoTask($taskID, $_SESSION['usr']);
-					}
-					else if(isset($_POST['markAsComplete']))
-					{
-						markTaskAsComplete($taskID, $_SESSION['usr']);
-					}
-					else if(isset($_POST['notComplete']))
-					{
-						markTaskAsNotComplete($taskID);
-					}
-					$_SESSION['usr'] = getUserData($con, $_SESSION['usr']['username']);
-					
-					/* this needs to be in the sidebarrrrrrrrrrrrrrrrrr */
-					if($ideaMember == 0)
-					{
-	                    echo '<form method="post" action="'; 
-	                    echo $PHP_SELF; 
-	                    echo '">
-	                    <input type="submit" ';
-	                    if(!userIsDoingTask($taskID, $_SESSION['usr']))
-	                    {
-	                    	echo 'name="doTask" value="I\'ll help do this task!">';
-	                    }
-	                    else
-	                    {
-	                    	echo 'name="undoTask" value="I can\'t do this task anymore!"><br>';
-	                    	echo '<input type="submit" ';
-	                    	if(!taskIsComplete($taskID))
-	                    	{
-	                    		echo 'name="markAsComplete" value="I\'ve completed this task!"><br>';
-	                    	}
-	                    	else
-	                    	{
-	                    		echo 'name="notComplete" value="This task is not complete yet!"><br>';
-	                    	}
-	                    	
-	                    	/* 
-                    		need some visual indicator both on the todolist and here to show whether the task is complete or not
-	                    	*/
-	                    }
-	                    echo '</form>';
+	                echo '<form method="post" action="'; 
+	                echo $PHP_SELF; 
+	                echo '">
+	                <input type="submit" ';
+	                if(!userIsDoingTask($taskID, $_SESSION['usr']))
+	                {
+	                	echo 'name="doTask" value="I\'ll help do this task!">';
 	                }
-	                displaySingleTask($taskID);
-				}
+	                else
+	                {
+	                	echo 'name="undoTask" value="I can\'t do this task anymore!"><br>';
+	                	echo '<input type="submit" ';
+	                	if(!taskIsComplete($taskID))
+	                	{
+	                		echo 'name="markAsComplete" value="I\'ve completed this task!"><br>';
+	                	}
+	                	else
+	                	{
+	                		echo 'name="notComplete" value="This task is not complete yet!"><br>';
+	                	}
+	                	
+	                	/* 
+	            		need some visual indicator both on the todolist and here to show whether the task is complete or not
+	                	*/
+	                }
+	                echo '</form>';
+	            }
 
-			?>
+			echo 
+			'</div>';
+    displaySingleTask($taskID);
+}
+
+			echo '
         </div>
     </div>
-</div>
-<?php
+</div>';
 include 'footer.php';
 
 ?>
