5510073cf07a831ae2c0c70840da1dd184ca03fa
Henry Coggin <henry-coggin@hotmail.co.uk>
2013-04-16 16:51:30 +0100

    dmy

--- functions_idea.php
+++ functions_idea.php
@@ -48,7 +48,7 @@ function outputIdeas(&$ideas)
 		$createdBy = $ideasArray['createdBy'];
 		$dateCreated = $ideasArray['dateCreated'];
 		var_dump($dateCreated);
-		$dateCreated = date("d-m-Y", $dateCreated);
+		$dateCreated = date("d m Y", $dateCreated);
 		var_dump($dateCreated);
 		$iID = $ideasArray['ideaID'];
 		$iVotes = $ideasArray['upVotes'];
