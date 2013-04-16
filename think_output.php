754fa4b350d6134a86959ce452d0df9568a680a1
Tom Nash <tommynosh@hotmail.com>
2013-04-16 15:29:48 +0100

    commit

--- think_output.php
+++ think_output.php
@@ -8,6 +8,9 @@ include 'functions_idea.php';
 <div class="clear"></div>
         <div id="post-container">
         	<div class="sidebar">
+        		We've handpicked these ideas for you based on your interests!
+        		<br>
+        		:)
           	</div>
           <div class="mainRight">
                 <?php think($con); ?>
