b36986dd3420e7ec49cea431144b4b683b6045d0
Tom Nash <tommynosh@hotmail.com>
2013-04-25 18:12:38 +0100

    messaging functions

--- functions_messaging.php
+++ functions_messaging.php
@@ -1,5 +1,42 @@
 <?php
 
+function sendMessage($toUser, $subject, $message)
+{
+	$sql = "INSERT INTO messages (fromUser, toUser, subject, content) 
+	VALUES ('".$_SESSION['usr']['username']."', '".$toUser."', '".$subject."', '".$content."')";
+	mysql_query($sql) or die(mysql_error());
+}
 
+function checkForNewMessages()
+{
+	$count = 0;
+	$sql = "SELECT * FROM messages WHERE toUser='".$_SESSION['usr']."' AND msgRead=0";
+	$res = mysql_query($sql) or die(mysql_error());
+	$newMsgs = mysql_fetch_array($res);
+	if($newMsgs != NULL)
+	{
+		$count = 1;
+		while(mysql_fetch_array($res))
+		{
+			$count++;
+		}
+	}
+	return $count;
+}
+
+function getAllMessages()
+{
+	$sql = "SELECT * FROM messages WHERE toUser='".$_SESSION['usr']."'";
+	$res = mysql_query($sql) or die(mysql_error());
+	return $res;
+}
+
+function displayMessages($resultSet)
+{
+	while(mysql_fetch_array($resultSet))
+	{
+		//display messages
+	}
+}
 
 ?>
