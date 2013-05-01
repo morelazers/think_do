933470bced14c9ae36d974a8bd8c6fc83c0f6047
Tom Nash <tommynosh@hotmail.com>
2013-05-02 00:49:13 +0100

    show the current values in the form when you go to edit

--- functions_gatherings.php
+++ functions_gatherings.php
@@ -138,10 +138,10 @@ function markAsNotAttending($gID)
 
 function updateGathering($gath, $ID)
 {
-	$gDesc = mysql_real_escape_string($gath['gatheringDescription']);
-	$gLoc = mysql_real_escape_string($gath['gatheringLocation']);
-	$date = $gath['gatheringDate'];
-	$time = $gath['gatheringTime'];
+	$gDesc = mysql_real_escape_string($gath['gathDescription']);
+	$gLoc = mysql_real_escape_string($gath['gathLocation']);
+	$date = $gath['gathDate'];
+	$time = $gath['gathTime'];
 	$sql = "UPDATE gatherings SET gathDescription='".$gDesc."', gathLocation='".$gLoc."', gathDate='".$date."', gathTime='".$time."' WHERE gathID=".$ID;
 	mysql_query($sql) or die(mysql_error());
 }
@@ -160,21 +160,21 @@ function currentUserIsGatheringProposer($u, $g)
 function showGatheringForm($g)
 {
 	echo '<div id="gathForm"><form method="post" action="#tabs-3">
-    <label for="gatheringDescription">What should the gathering accomplish?</label><br>
-    <input type="text" name="gatheringDescription" id="gatheringDescription" value="';
-    echo $g['gatheringDescription'];
+    <label for="gathDescription">What should the gathering accomplish?</label><br>
+    <input type="text" name="gathDescription" id="gathDescription" value="';
+    echo $g['gathDescription'];
     echo '"><br>
-	<label for="gatheringLocation">Where will it be?</label><br>
-    <input type="text" name="gatheringLocation" id="gatheringLocation" value="';
-    echo $g['gatheringLocation'];
+	<label for="gathLocation">Where will it be?</label><br>
+    <input type="text" name="gathLocation" id="gathLocation" value="';
+    echo $g['gathLocation'];
     echo '"><br>
-	<label for="gatheringDate">On which date?</label><br>
-    <input type="date" name="gatheringDate" id="gatheringDate" value="';
-    echo $g['gatheringDate'];
+	<label for="gathDate">On which date?</label><br>
+    <input type="date" name="gathDate" id="gathDate" value="';
+    echo $g['gathDate'];
     echo '"><br>
-    <label for="gatheringTime">At what time?</label><br>
-    <input type="time" name="gatheringTime" id="gatheringTime" value="';
-    echo $g['gatheringTime'];
+    <label for="gathTime">At what time?</label><br>
+    <input type="time" name="gathTime" id="gathTime" value="';
+    echo $g['gathTime'];
     echo '"><br>
     <input type="submit" name="submitGathering" value="Submit">
     </form></div><br><br>';
