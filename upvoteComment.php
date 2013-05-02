e943eebad1c79f4f501ce0944654b210cc57b441
Tom Nash <tommynosh@hotmail.com>
2013-05-02 16:59:40 +0100

    ):

--- upvoteComment.php
+++ upvoteComment.php
@@ -5,12 +5,13 @@ include "functions_comment.php";
 
 
 $upComID = $_GET['upCom'];
-var_dump($upComID);
 
 /*var_dump($upComID);*/
 
 $commentToUpvote = getCommentData($upComID);
 var_dump($commentToUpvote);
+echo '<br>';
+var_dump($_SESSION['usr']);
 
 incrementCommentUpvotes($commentToUpvote, $_SESSION['usr']);
 
